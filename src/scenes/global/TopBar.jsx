bcdbaeb12cebc6338d821ceff0ac4067a95b2b38
Eldiwr <Eldiwr97@gmail.com>
2023-01-29 16:32:39 +0200

    Complited mui theme settings

---
+++ src/scenes/global/TopBar.jsx
@@ -0,0 +1,16 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import { LightModeOutlined } from "@mui/icons-material";
+
+
+
+
+const TopBar = () => {
+    return (
+        <div>TopBar</div>
+    )
+}
+
+export default TopBar;
